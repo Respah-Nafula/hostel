bb43bf245ea035b76ee3ca7a2210f6fbff34e7b7
Your Name <nafularespah@gmail.com>
2022-08-20 22:15:11 +0300

    changes

--- hostelApp/admin.py
+++ hostelApp/admin.py
@@ -1,21 +1,43 @@
+from unicodedata import name
 from django.contrib import admin
 # Register your models here.
 # from tkinter import N
 
-from .models import Admin,StudentDetails,Classroom,RoomAllotment,StudentRegistration,StudentsGuardian,Receipt,RoomFees,ClearanceForm,ClearanceMessage,Notification
+from .models import Admins, Room_details,Student_Registration,Guardian_Detail,Receipt,Notification,Clearance
  
 
-class Admin(admin.ModelAdmin):
-    list_display=("first_name","last_name","age","email")
-    search_fields=("first_name","last_name")
+class AdminstrationAdmin(admin.ModelAdmin):
+    list_display=("name","password","email")
+    search_fields=("email","name")
+    admin.site.register(Admins)
 
-admin.site.register(Admin)
-admin.site.register(StudentDetails)
-admin.site.register(Classroom)
-admin.site.register(RoomAllotment)
-admin.site.register(StudentRegistration)
-admin.site.registe(Receipt)
-admin.site.register(RoomFees)
-admin.site.register(ClearanceForm)
-admin.site.register(ClearanceMessage)
-admin.site.register(Notification)
+class Room_detailsAdmin(admin.ModelAdmin):
+     list_display=("room-Name","room_Location","number_of_beds","numberOfStudents","status","room_Number")
+     search_fields=("room-Name","room_Location","number_of_beds","numberOfStudents","status","room_Number")
+     admin.register(Room_details)
+
+class Student_RegistrationAdmin(admin.ModelAdmin):
+     list_display=("student_Name","dob","gender","status","admission_Date","phonenumber",'id_number',"parent_Name","parent_contact")
+     search_fields=("student_Name","dob","gender","status","admission_Date","phonenumber",'id_number',"parent_Name","parent_contact")
+     admin.register(Student_Registration)
+
+class Guardian_DetailAdmin(admin.ModelAdmin):
+    list_display=("name","id_number","email_address","address","phone","admission_date")
+    search_fields=("name","id_number","email_address","address","phone","admission_date")
+    admin.register(Guardian_Detail)
+
+class ReceiptAdmin(admin.ModelAdmin):
+    list_display=("stud_Name","student_Admission_Number","amount","course","signature","dateOfPayment")
+    search_fields=("stud_Name","student_Admission_Number","amount","course","signature","dateOfPayment")
+    admin.register(Receipt)
+
+class NotificationAdmin(admin.ModelAdmin):
+     list_display=("name","studentId","email_address","description","institution","notification_Origin", "notification_Destination","dateOfnotification")
+     search_fields=("name","studentId","email_address","description","institution","notification_Origin", "notification_Destination","dateOfnotification")
+     admin.register(Notification)
+class ClearanceAdmin(admin.ModelAdmin):
+    list_display=("name","admission_Number","email_address","completion_date","phonenumber","admission_date")
+    search_fields=("name","admission_Number","email_address","completion_date","phonenumber","admission_date")
+    admin.register(Clearance)
+
+
--- hostelApp/models.py
+++ hostelApp/models.py
@@ -5,73 +5,78 @@ from django.db import models
 
 # Create your models here.
 
-class Admin(models.Model):
-    Name=models.CharField(max_length=20)
-    email=models.EmailField()
-    password=models.IntegerField()
-    phone_number=models.CharField(max_length=10)
+class Admins(models.Model):
+  name=models.CharField(max_length=20)
+  email=models.EmailField()
+  password=models.IntegerField()
+  phone_number=models.CharField(max_length=10)
 
 
-class Room(models.Model):
- Room_Name=models.CharField(max_length=25)
- room_Location=models.CharField()
- Number_Of_Beds=models.IntegerField()
- NumberOfStudents=models.IntegerField()
- status=models.BooleanField() 
- Room_Number=models.IntegerField()
+class Room_details(models.Model):
+  room_Name=models.CharField(max_length=25)
+  room_Location=models.CharField(max_length=50)
+  number_Of_Beds=models.IntegerField()
+  numberOfStudents=models.IntegerField()
+  status=models.BooleanField() 
+CHOICES=(
+         ("M","occupied"),
+      ("F","Not occupied"),
+ )
+
+room_Number=models.IntegerField()
  
 class Student_Registration(models.Model):
-    Stud_Name=models.CharField(max_length=25)
-    DOB=models.DateTimeField(default=datetime.now)
-    GENDER_CHOICES=(
+  stud_Name=models.CharField(max_length=25)
+  dob=models.DateTimeField(default=datetime.now)
+  GENDER_CHOICES=(
         ("M","MALE"),
         ("F","FEMALE"),
     )
-    Gender=models.BooleanField()
+  gender=models.BooleanField()
     
-    status=models.BooleanField()
-    admission_Date=models.DateTimeField()
-    phonenumber=models.CharField()
-    studentAdmissionNumber=models.CharField()
-    Id_number=models.CharField()
-    Parent_Name=models.CharField(max_length=20)
-    Parent_Contact=models.CharField()
+  status=models.BooleanField()
+  admission_Date=models.DateTimeField()
+  phonenumber=models.CharField(max_length=10)
+  studentAdmissionNumber=models.CharField(max_length=10)
+  id_number=models.CharField(max_length=15)
+  parent_Name=models.CharField(max_length=20)
+  parent_Contact=models.CharField(max_length=10)
 
 
 
-class Guardian_Details(models.Models):
-        Name=models.CharField(max_length=20)
-        id_number=models.CharField()
-        Email_address=models.EmailField()
-        address=models.CharField()
-        phonenumber=models.CharField(max_length=15)
-        admission_date=models.DateTimeField(default=datetime.now)
+class Guardian_Detail(models.Model):
+  name=models.CharField(max_length=20)
+  id_number=models.CharField(max_length=20)
+  email_address=models.EmailField()
+  address=models.CharField(max_length=15)
+  phonenumber=models.CharField(max_length=15)
+  admission_date=models.DateTimeField(default=datetime.now)
 
 class Receipt(models.Model):
-         Stud_Name=models("Receipt",on_delete=models.CASCADE,related_name='Student_Registration_Receipt')
-         studentAdmissionNumber=models("Receipt",on_delete=models.CASCADE,related_name='StudAdmissionNumber_Receipt')
-         amount=models.IntegerField()
-         course=models.CharField()
-         signature=models.ImageField()
-         dateOfPayment=models.DateTimeField(default=datetime.now)
+   stud_Name=models.ForeignKey("Receipt",on_delete=models.CASCADE,related_name='Student_Registration_Receipt')
+   studentAdmissionNumber=models.ForeignKey("Receipt",on_delete=models.CASCADE,related_name='StudAdmissionNumber_Receipt')
+   amount=models.IntegerField()
+   course=models.CharField(max_length=20)
+   signature=models.ImageField()
+   dateOfPayment=models.DateTimeField(default=datetime.now)
 
-class clearance (models.Model):
-            Name=models("Clearance",on_delete=models.CASCADE,related_name='Student_Registration_Clearance')
-            AdmissionNumber=models("Clearance",on_delete=models.CASCADE,related_name='Stud_Registration_Clearance')
-            Email_address=models("Clearance",on_delete=models.CASCADE,related_name='Stud_Registration_Clearance')
-            Completion_date=models.DateTimeField(default=datetime.now)
-            phonenumber=models("Clearance",on_delete=models.CASCADE,related_name='Stud_Registration_Clearance')
-            admission_Date=models("Clearance",on_delete=models.CASCADE,related_name='Stud_Registration_Clearance')
+class Clearance(models.Model):
+   name=models.CharField(max_length=20)
+   admissionNumber=models.CharField(max_length=15)
+   email_address=models.EmailField()
+   completion_date=models.DateTimeField(default=datetime.now)
+   phonenumber=models.CharField(max_length=10)
+   admission_Date=models.DateTimeField(default=datetime.now)
 
-class Notification(models.Models):
-                Name=models("Clearance",on_delete=models.CASCADE,related_name='Stud_Registration_Clearance')
-                studentId=models("Clearance",on_delete=models.CASCADE,related_name='Stud_Registration_Clearance')
-                Email_address=models("Clearance",on_delete=models.CASCADE,related_name='Stud_Registration_Clearance')
-                description=models.CharField()
-                institution=models.CharField()
-                Notification_Origin=models.CharField
-                Notification_Destination=models.CharField()
-                DateOfnotification=models.DateTimeField(default=datetime.now)
+class Notification(models.Model):
+   name=models.CharField(max_length=20)
+   studentId=models.CharField(max_length=15)
+   email_address=models.EmailField()
+   description=models.CharField(max_length=50)
+   institution=models.CharField(max_length=20)
+   notification_Origin=models.CharField(max_length=15)
+   notification_Destination=models.CharField(max_length=15)
+   dateOfnotification=models.DateTimeField(default=datetime.now)
 
 
 
